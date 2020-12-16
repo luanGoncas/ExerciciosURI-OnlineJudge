3460a025c1ae8aaa10d1f578006332cb6aa26c54
Luan Santos Gonçalves <39390414+luanGoncas@users.noreply.github.com>
2020-12-16 15:26:04 -0300

    Update Exercicio1789.py

--- ExercíciosURI/Exercicio1789/Exercicio1789.py
+++ ExercíciosURI/Exercicio1789/Exercicio1789.py
@@ -1,35 +1,6 @@
 # -*- coding: utf-8 -*-
-
-while True:
-	try:
-		L = int(input())
-		V = input().split()
-
-		entrada = [int(entrada) for entrada in V]
-
-		entrada.sort()
-		lesmaRapida = entrada[len(entrada) - 1]
-
-		"""
-		if lesmaRapida < 10:
-			print("1")
-		elif lesmaRapida >= 10 and lesmaRapida < 20:
-			print("2")
-		else:
-			print("3")
-		"""
-	except EOFError:
-		break
-	if lesmaRapida < 10:
-		print("1")
-	elif lesmaRapida >= 10 and lesmaRapida < 20:
-		print("2")
-	else:
-		print("3")
-
 # SOLUÇÃO DO PROBLEMA COM ARQUIVO
 
-"""
 arquivo = open("/home/luan/Documentos/ExercíciosURI/Exercicio1789/entrada.txt", "r")
 
 L = arquivo.readline()
